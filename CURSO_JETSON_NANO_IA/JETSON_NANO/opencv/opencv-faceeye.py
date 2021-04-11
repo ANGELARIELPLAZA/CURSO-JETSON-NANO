6c3c1ec5df30bb6066762b560c8cd380a96ceb0b
ANGELARIELPLAZA <75581459+ANGELARIELPLAZA@users.noreply.github.com>
2021-04-11 16:13:22 -0500

    cambios

--- CURSO_JETSON_NANO_IA/JETSON_NANO/opencv/opencv-faceeye.py
+++ CURSO_JETSON_NANO_IA/JETSON_NANO/opencv/opencv-faceeye.py
@@ -20,12 +20,12 @@ while True:
     grey=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
     faces=face_cascade.detectMultiScale(grey,1.3,5)
     for(x,y,w,h) in faces:
-        cv2.rectangle(frame,(x,y),(x+w,y+h), (0,0,255),2)
+        cv2.rectangle(frame,(x,y),(x+w,y+h), (255,0,0),2)
         roigrey=grey[y:y+h,x:x+w]
         roicolor=frame[y:y+h,x:x+w]
         eyes=eye_cascade.detectMultiScale(roigrey)
         for (xEye,yEye,wEye,hEye) in eyes:
-            cv2.circle(roicolor,(int(xEye+wEye/2),int(yEye+hEye/2)),8,(255,0,0),-1)
+            cv2.rectangle(roicolor,(xEye,yEye),(xEye+wEye),(0,0,255),21)
  
     cv2.imshow('nanoCam',frame)
     cv2.moveWindow('nanoCam',0,0)
