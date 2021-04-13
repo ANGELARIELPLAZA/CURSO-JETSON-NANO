e9e5a30ef2413a13ee587da67c5f31a90316126a
ANGELARIELPLAZA <75581459+ANGELARIELPLAZA@users.noreply.github.com>
2021-04-13 12:11:05 -0500

    arduino

---
+++ prueba4.py
@@ -0,0 +1,25 @@
+import cv2
+import dlib
+
+detector=dlib.get_frontal_face_detector()
+
+predictor=
+dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
+cap=cv2.VideoCapture()
+
+while True:
+    _,frame=cap.read()
+    gray.=cv2.cvtCOLOR(frame,cv2.COLOR_BGR2GRAY)
+    faces=detector(gray)
+    for face in faces:
+        x1=face.left()
+        y1=face.top()
+        x2=face.right()
+        y2=face.bottom()
+
+        landmarks=predictor(gray,face)
+        for n in range (0,68):
+            x=landmarks.part(n).x   
+            y=landmarks.part(n).y
+            cv2.circle(frame,(x,y),3,(0,255,0),1)
+            cv2.imshow('FACES',frame)
